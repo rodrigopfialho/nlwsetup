5b4ecf959a3d8c274db8e0c9755cdc4e38ea31e7
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-01-18 21:26:24 -0300

    server

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habit = await prisma.habit.findMany()
+ 
+    return habit
+    
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP SERVER running!')
+})
