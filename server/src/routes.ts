29a73d78f8dadbac9aff363fbd53311cd2c9172f
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-01-22 20:45:40 -0300

    create relations

---
+++ server/src/routes.ts
@@ -0,0 +1,61 @@
+import { FastifyInstance } from "fastify"
+import dayjs from 'dayjs'
+import { z } from 'zod';
+import { prisma } from "./lib/prisma"
+
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/habits', async (request) => {
+        const createhabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(z.number().min(0).max(6))
+        })
+
+        const {title, weekDays} = createhabitBody.parse(request.body)
+
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                }
+            }
+        })
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const {date} = getDayParams.parse(request.query)
+
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        return {
+            possibleHabits,
+        }
+    })
+}
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./routes";
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/hello', async () => {
-    const habit = await prisma.habit.findMany()
- 
-    return habit
-    
-})
 
 app.listen({
     port: 3333,
