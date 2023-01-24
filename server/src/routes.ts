8b2b0084f6175e29c282a1469192bde208b79133
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-01-23 21:56:13 -0300

    implementação das rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -53,8 +53,22 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
 
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
         return {
             possibleHabits,
+            completedHabits
         }
     })
 }
