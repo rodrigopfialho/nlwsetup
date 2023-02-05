45faf6a4c92751e0a150af8554c069eac4bca86c
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-02-05 11:15:01 -0300

    compomentes

--- web/src/components/habitDay.tsx
+++ web/src/components/habitDay.tsx
@@ -1,9 +1,24 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
 // interface HabitDayPros {
 //     completed?: number
 // }
 
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+            <Popover.Portal>
+                <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                    <span className='font-semibold text-zinc-700'>Terça-feira</span>
+                    <span className='mt-1 font-extrabold leading-ti text-3xl'>17/01</span>
+                    <ProgressBar />
+                    
+
+                    <Popover.Arrow  height={8} width={16} className='fill-zinc-900'/>
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
     )
 }
