fa43c5663126ace262a67fd44925b813a660312a
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-02-05 19:22:44 -0300

    aula 3

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,28 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const {Navigator, Screen} = createNativeStackNavigator()
+
+import {Home} from '../screen/Home'
+import {New} from '../screen/New'
+import {Habit} from '../screen/Habit'
+
+export function AppRoutes() {
+    return (
+        <Navigator screenOptions={{ headerShown: false}}>
+            <Screen 
+                name="home"
+                component={Home}
+            />
+
+            <Screen 
+                name="new"
+                component={New}
+            />
+
+            <Screen 
+                name="habit"
+                component={Habit}
+            />
+        </Navigator>
+    )
+}
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,11 +1,18 @@
-export function ProgressBar() {
+interface ProgressBarProps {
+    progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
     return (
         <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4 '>
             <div
-                className='h-3 rounded-xl bg-violet-600 w-3/4'
                 role="progressbar"
                 aria-label="Progresso de hábitos completados nesse dia"
-                arial-aria-valuenow={75}
+                arial-aria-valuenow={props.progress}
+                className='h-3 rounded-xl bg-violet-600'
+                style={{
+                    'width': `${props.progress}%`
+                }}        
             />
         </div>
     )
