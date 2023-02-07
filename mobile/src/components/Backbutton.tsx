ac5dcbed8c98eeb299068f6a5587bb182da1bcbd
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2023-02-06 21:09:13 -0300

    implementação de funcionalidades

--- mobile/src/components/Backbutton.tsx
+++ mobile/src/components/Backbutton.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native'
 
 export function BackButton() {
     const {goBack} = useNavigation()
+    
     return (
         <TouchableOpacity
             activeOpacity={0.7}
